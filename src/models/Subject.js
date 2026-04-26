fda35f26044b6c6a2d88e693a64978248bc5c213
ashutosh singh <ashutoshlapt@gmail.com>
2026-04-26 13:31:45 +0530

    create server and utils

--- src/models/Subject.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const SubjectSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true }, // e.g. "Math", "Physics"
-  description: { type: String },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("Subject", SubjectSchema);
--- src/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-
-router.post("/mock-google-login", authController.mockGoogleLogin);
-
-module.exports = router;
