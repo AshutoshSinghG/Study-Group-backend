e9fa4642fa87a67033d0420f4fe6701f72cc2d05
ashutosh singh <ashutoshlapt@gmail.com>
2026-04-27 00:48:16 +0530

    Add Group Routes

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { createGroup, addMember } = require('../controllers/groupController');
 const { protect } = require('../middleware/auth');
-const { addGroupGoal } = require('../controllers/goalController');
+const { createGoal } = require('../controllers/goalController');
 const { recordActivity } = require('../controllers/activityController');
 const { getLeaderboard } = require('../controllers/leaderboardController');
 const { getGroupProgress } = require('../controllers/progressController');
@@ -10,7 +10,7 @@ const { getGroupProgress } = require('../controllers/progressController');
 router.post('/', protect, createGroup);
 router.post('/:id/member', protect, addMember);
 
-router.post('/:id/goal', protect, addGroupGoal);
+router.post('/:id/goal', protect, createGoal);
 router.post('/:id/activity', protect, recordActivity);
 
 router.get('/:id/leaderboard', protect, getLeaderboard);
